263295a509d3be0c8827718cf41329febfd2d385
ritikaaswal108-wq <ritikaaswal108@gmail.com>
2026-09-13 12:42:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Himalayan dental clinic website
+console.log("Website loaded successfully!")
+document.addEventListener("DOMContentLoaded", function(){
+
+
+alert("welcome to Himalayan dental clinic");
+});
